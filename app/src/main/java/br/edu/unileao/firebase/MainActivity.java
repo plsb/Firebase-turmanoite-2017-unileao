6b6df499a7b86577d7fa01d4e6fb4a62607755de
pelusb <peluissaraiva@gmail.com>
2017-05-22 21:03:49 -0300

    tela salvar firebase

--- app/src/main/java/br/edu/unileao/firebase/MainActivity.java
+++ app/src/main/java/br/edu/unileao/firebase/MainActivity.java
@@ -2,6 +2,9 @@ package br.edu.unileao.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,17 +14,46 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference databaseReference = FirebaseDatabase.
                                                         getInstance().getReference();
     private DatabaseReference clientesReference = databaseReference.child("clientes");
-
+    private EditText edtNome;
+    private Button btSalvar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Cliente cliente = new Cliente();
-        cliente.setNome("José");
-        cliente.setEndereco("Rua D");
-        cliente.setCpf("9944169");
+        edtNome = (EditText) findViewById(R.id.edtNome);
+        btSalvar = (Button) findViewById(R.id.btSalvar);
 
-        clientesReference.child("02").setValue(cliente);
+        btSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cliente cliente = new Cliente();
+                cliente.setNome(edtNome.getText().toString());
+                cliente.setEndereco("Rua D");
+                cliente.setCpf("9944169");
+
+                clientesReference.child("03").setValue(cliente);
+
+                edtNome.setText("");
+            }
+        });
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
