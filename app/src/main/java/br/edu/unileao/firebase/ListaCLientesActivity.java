2e1a314cf981e4205a52a509755deb58f10aaff0
pelusb <peluissaraiva@gmail.com>
2017-05-29 21:23:59 -0300

    listagem de clientes

--- app/src/main/java/br/edu/unileao/firebase/ListaCLientesActivity.java
+++ app/src/main/java/br/edu/unileao/firebase/ListaCLientesActivity.java
@@ -3,7 +3,17 @@ package br.edu.unileao.firebase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by pelusb on 29/05/17.
@@ -11,9 +21,62 @@ import android.widget.ListView;
 
 public class ListaCLientesActivity extends AppCompatActivity {
 
+    private DatabaseReference databaseReference = FirebaseDatabase.
+            getInstance().getReference();
+    private DatabaseReference clientesReference =
+                        databaseReference.child("clientes");
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ListView listView = (ListView) findViewById(R.id.listaCliente);
+        setContentView(R.layout.activity_lista_cliente);
+
+        final ListView listView = (ListView) findViewById(R.id.listaCliente);
+
+        clientesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> dsCliente = dataSnapshot.getChildren();
+                List<Cliente> clientes = new ArrayList<Cliente>();
+                while(dsCliente.iterator().hasNext()){
+                    DataSnapshot dsObjetoCliente =
+                            dsCliente.iterator().next();
+                    Cliente cliente =
+                            dsObjetoCliente.getValue(Cliente.class);
+                    clientes.add(cliente);
+                }
+                ArrayAdapter<Cliente> adapter =
+                        new ArrayAdapter<Cliente>(ListaCLientesActivity.this,
+                                    android.R.layout.simple_list_item_1,
+                                clientes);
+                listView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
